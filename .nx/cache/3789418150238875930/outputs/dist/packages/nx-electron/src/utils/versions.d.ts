feffef7b0340a9e10f228fb7c1d95e13d9d0de13
Dominic Rico Gómez <hello@coderocket.me>
2023-11-28 14:56:20 +0100

    build it

---
+++ .nx/cache/3789418150238875930/outputs/dist/packages/nx-electron/src/utils/versions.d.ts
@@ -0,0 +1,2 @@
+export declare const nxElectronVersion = "^16.1.0";
+export declare const electronVersion = "^27.0.4";
---
+++ dist/packages/nx-electron/src/utils/versions.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.electronVersion = exports.nxElectronVersion = void 0;
+exports.nxElectronVersion = '^16.1.0';
+exports.electronVersion = '^27.0.4';
+//# sourceMappingURL=versions.js.map
